760c1dd1b0a5a23e4a533203362e370e69a07589
azaria-dedmon <53963370+azaria-dedmon@users.noreply.github.com>
2020-10-07 15:31:16 -0400

    Update food.js

--- samopage/food.js
+++ samopage/food.js
@@ -1,38 +1,22 @@
 
-function foodInfoToggle1() {
+const foodInfoToggle1 = () => {
     const foodInfo = document.querySelector('#first');
-    if (foodInfo.style.display === "none") {
-      foodInfo.style.display = "block";
-    } else {
-        foodInfo.style.display = "none";
-    }
-  }
-
-  function foodInfoToggle2() {
-    let foodInfo2 = document.querySelector('#second');
-    if (foodInfo2.style.display === "none") {
-      foodInfo2.style.display = "block";
-    } else {
-      foodInfo2.style.display = "none";
-    }
-  }
-
-
-function foodInfoToggle3() {
-    let foodInfo3 = document.querySelector('#third')
-    if (foodInfo3.style.display === "none") {
-      foodInfo3.style.display = "block";
-    } else {
-      foodInfo3.style.display = "none";
-    }
+    foodInfo.style.display === "none" ? foodInfo.style.display = "block" :  foodInfo.style.display = "none";
 }
 
-function foodInfoToggle4() {
+const foodInfoToggle2 = () => {
+    let foodInfo2 = document.querySelector('#second');
+    foodInfo2.style.display === "none" ? foodInfo2.style.display = "block" :  foodInfo2.style.display = "none"
+  }
+
+
+const foodInfoToggle3 = () => {
+    let foodInfo3 = document.querySelector('#third')
+    foodInfo3.style.display === "none"? foodInfo3.style.display = "block" : foodInfo3.style.display = "none"
+}
+
+const foodInfoToggle4 = () => {
     let foodInfo4 = document.querySelector('#fourth')
-    if (foodInfo4.style.display === "none") {
-      foodInfo4.style.display = "block";
-    } else {
-      foodInfo4.style.display = "none";
-    }
+    foodInfo4.style.display === "none" ? foodInfo4.style.display = "block" : foodInfo4.style.display = "none"
 }
 
